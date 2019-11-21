e35415b8eca90083f490024e246e8bfe8ef0d4ac
Jean-François Larente <jfl@sitecore.net>
2019-11-21 15:30:39 -0500

    Work in progress - adding ability to apply transforms in Watch-Directory.ps1

--- windows/9.3.0/sitecore-assets/tools/scripts/Watch-Directory.ps1
+++ windows/9.3.0/sitecore-assets/tools/scripts/Watch-Directory.ps1
@@ -2,11 +2,11 @@
 param(
     # Path to watch for changes
     [Parameter(Mandatory = $true)]
-    [ValidateScript( {Test-Path $_ -PathType 'Container'})] 
+    [ValidateScript( { Test-Path $_ -PathType 'Container' })]
     [string]$Path,
     # Destination path to keep updated
     [Parameter(Mandatory = $true)]
-    [ValidateScript( {Test-Path $_ -PathType 'Container'})] 
+    [ValidateScript( { Test-Path $_ -PathType 'Container' })]
     [string]$Destination,
     # Milliseconds to sleep between sync operations
     [Parameter(Mandatory = $false)]
@@ -22,11 +22,32 @@ param(
     [array]$DefaultExcludedDirectories = @("obj", "Properties", "node_modules"),
     # Additional directories to skip during sync
     [Parameter(Mandatory = $false)]
-    [array]$ExcludeDirectories = @()
+    [array]$ExcludeDirectories = @(),
+    # Determine whether xdt transforms should be processed
+    [switch]$TransformXdts,
+    # Root path where config transforms are located. Defaults to root of src (will traverse entire src tree)
+    [Parameter(Mandatory = $false)]
+    [array]$TransformSourcePath = @("c:\src")
 )
 
-function Sync
-{
+function ApplyTransforms {
+    param(
+        [Parameter(Mandatory = $true)]
+        $transformSourcePath,
+        [Parameter(Mandatory = $true)]
+        $destination
+    )
+
+    Get-ChildItem -Path $transformSourcePath -Filter *.xdt -Recurse | Foreach-Object {
+        $target = (Join-Path $destination (($_.FullName | Resolve-Path -Relative) -replace '.xdt' , ''))
+        $source = $_.FullName
+        Write-Host ("{0}: Applying Transform: {1}" -f [DateTime]::Now.ToString("HH:mm:ss:fff"), $target )
+        & "C:\tools\scripts\Invoke-XdtTransform.ps1" -Path $target -XdtPath $source -XdtDllPath 'C:\tools\bin\Microsoft.Web.XmlTransform.dll'
+        Write-Host ("{0}: Deleting: {1}" -f [DateTime]::Now.ToString("HH:mm:ss:fff"), $source )
+        Remove-Item $_
+    }
+}
+function Sync {
     param(
         [Parameter(Mandatory = $true)]
         $Path,
@@ -39,9 +60,8 @@ function Sync
     )
 
     $command = @("robocopy", "`"$Path`"", "`"$Destination`"", "/E", "/XX", "/MT:1", "/NJH", "/NJS", "/FP", "/NDL", "/NP", "/NS", "/R:5", "/W:1")
-    
-    if ($ExcludeDirectories.Count -gt 0)
-    {        
+
+    if ($ExcludeDirectories.Count -gt 0) {
         $command += "/XD "
 
         $ExcludeDirectories | ForEach-Object {
@@ -51,8 +71,7 @@ function Sync
         $command = $command.TrimEnd()
     }
 
-    if ($ExcludeFiles.Count -gt 0)
-    {        
+    if ($ExcludeFiles.Count -gt 0) {
         $command += "/XF "
 
         $ExcludeFiles | ForEach-Object {
@@ -61,7 +80,7 @@ function Sync
 
         $command = $command.TrimEnd()
     }
-    
+
     $commandString = $command -join " "
 
     $dirty = $false
@@ -70,20 +89,23 @@ function Sync
         $line = $_.Trim().Replace("`r`n", "").Replace("`t", " ")
         $dirty = ![string]::IsNullOrEmpty($line)
 
-        if ($dirty)
-        {
-            Write-Host ("{0}: {1}" -f [DateTime]::Now.ToString("HH:mm:ss:fff"), $line) -ForegroundColor DarkGray            
+        if ($dirty) {
+            Write-Host ("{0}: {1}" -f [DateTime]::Now.ToString("HH:mm:ss:fff"), $line) -ForegroundColor DarkGray
         }
     }
 
-    if ($dirty)
-    {
+    if ($dirty) {
         Write-Host ("{0}: Done syncing..." -f [DateTime]::Now.ToString("HH:mm:ss:fff")) -ForegroundColor Green
     }
 }
 
 # Setup exclude rules
 $fileRules = ($DefaultExcludedFiles + $ExcludeFiles) | Select-Object -Unique
+
+# Don't sync xdt files to webroot if transformations are going to be applied
+if ($TransformXdts) {
+    $fileRules = ($fileRules + @("*.xdt")) | Select-Object -Unique
+}
 $directoryRules = ($DefaultExcludedDirectories + $ExcludeDirectories) | Select-Object -Unique
 
 Write-Host ("{0}: Excluding files: {1}" -f [DateTime]::Now.ToString("HH:mm:ss:fff"), ($fileRules -join ", "))
@@ -102,40 +124,35 @@ Register-ObjectEvent $watcher Deleted -SourceIdentifier "FileDeleted" -MessageDa
     $destinationPath = Join-Path $event.MessageData $eventArgs.Name
     $delete = !(Test-Path $eventArgs.FullPath) -and (Test-Path $destinationPath)
 
-    if ($delete)
-    {
-        try
-        {
+    if ($delete) {
+        try {
             Remove-Item -Path $destinationPath -Force -Recurse -ErrorAction "SilentlyContinue"
 
             Write-Host ("{0}: Deleted '{1}'..." -f [DateTime]::Now.ToString("HH:mm:ss:fff"), $destinationPath) -ForegroundColor Green
         }
-        catch
-        {
+        catch {
             Write-Host ("{0}: Could not delete '{1}'..." -f [DateTime]::Now.ToString("HH:mm:ss:fff"), $destinationPath) -ForegroundColor Red
         }
     }
 } | Out-Null
 
-try
-{
+try {
     Write-Host ("{0}: Watching '{1}' for changes, will copy to '{2}'..." -f [DateTime]::Now.ToString("HH:mm:ss:fff"), $Path, $Destination)
 
     # Main loop
-    while ($true)
-    {
+    while ($true) {
         Sync -Path $Path -Destination $Destination -ExcludeFiles $fileRules -ExcludeDirectories $directoryRules
-
+        if ($TransformXdts) {
+            ApplyTransforms -sourcePath $TransformSourcePath -destination $Destination
+        }
         Start-Sleep -Milliseconds $SleepMilliseconds
     }
 }
-finally 
-{
+finally {
     # Cleanup
     Get-EventSubscriber -SourceIdentifier "FileDeleted" | Unregister-Event
 
-    if ($watcher -ne $null)
-    {
+    if ($null -eq $watcher) {
         $watcher.Dispose()
         $watcher = $null
     }
